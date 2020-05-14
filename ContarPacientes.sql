2b187c5f297849b17c90f8ef8903e086421494e0
PabloG-Grumo08 <65315884+PabloG-Grumo08@users.noreply.github.com>
2020-05-14 23:10:10 +0200

    Functions
    Funciones para contar filas de las tablas Hospital del Mini Reto 3

---
+++ ContarPacientes.sql
@@ -0,0 +1,40 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Scalar Function (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Name
+-- Create date: 
+-- Description:	
+-- =============================================
+CREATE FUNCTION ContarPacientes 
+(
+	-- Add the parameters for the function here
+	 
+)
+RETURNS int
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @Result int
+
+	-- Add the T-SQL statements to compute the return value here
+	SELECT @Result = COUNT(NumHistorial) FROM [hospital].[Pacientes]
+
+	-- Return the result of the function
+	RETURN @Result
+
+END
+GO
+
---
+++ ContarPacientesGenero.sql
@@ -0,0 +1,42 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Scalar Function (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Name
+-- Create date: 
+-- Description:	
+-- =============================================
+CREATE FUNCTION ContarPacientesGenero 
+(
+	-- Add the parameters for the function here
+	@p1 int
+)
+RETURNS int
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @Result int
+
+	-- Add the T-SQL statements to compute the return value here
+	SELECT @Result =	COUNT(NumHistorial)
+						FROM [hospital].[Pacientes]
+						WHERE Sexo = @p1
+
+	-- Return the result of the function
+	RETURN @Result
+
+END
+GO
+
